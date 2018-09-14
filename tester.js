3da5960c095648f55725343ebc31e69ccb667098
a.nowakowski <a.nowakowski@devesoft.pl>
2018-09-14 15:25:42 +0200

    feat: add CRC

--- tester.js
+++ tester.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const {VM} = require('vm2');
+const { crc32 } = require('crc');
 
 const studentHash = process.argv[2] || '';
 const problems = ['problem001', 'problem002', 'problem003', 'problem004'];
@@ -9,14 +10,15 @@ for (let problem of problems) {
     let studentScriptPath = './student-scripts/' + problem + '.js';
     let testScriptPath = './test-scripts/' + problem + '.js';
     if (fs.existsSync(studentScriptPath) === false) {
-        result.push([problem, 'failed', [0, 0], 'The student\'s script does not exist.']);
+        result.push([problem, 'failed', [0, 0], 'The student\'s script does not exist.', '']);
         continue;
     }
     if (fs.existsSync(studentScriptPath) === false) {
-        result.push([problem, 'failed', [0, 0], 'The test\'s script does not exist.']);
+        result.push([problem, 'failed', [0, 0], 'The test\'s script does not exist.', '']);
         continue;
     }
     let studentScript = fs.readFileSync(studentScriptPath, 'utf8');
+    let studentScriptCRC = crc32(studentScript).toString(16);
     let testScript = fs.readFileSync(testScriptPath, 'utf8');
     try {
         let vm = new VM({
@@ -26,18 +28,18 @@ for (let problem of problems) {
         });
         let testResult = vm.run(studentScript + "\n" + testScript);
         if (testResult[1] > 0) {
-            result.push([problem, 'failed', testResult, 'The tests failed.']);
+            result.push([problem, 'failed', testResult, 'The tests failed.', studentScriptCRC]);
         } else {
-            result.push([problem, 'sucess', testResult, 'OK']);
+            result.push([problem, 'sucess', testResult, 'OK', studentScriptCRC]);
         }
     } catch (exception) {
-        result.push([problem, 'failed', [0, 0], exception.toString()]);
+        result.push([problem, 'failed', [0, 0], exception.toString(), studentScriptCRC]);
     }
 }
 
 console.log("Student hash: " + studentHash);
 for (let record of result) {
     console.log("Problem: " + record[0] + ", status: " + record[1] + ", tests: [" + record[2][0] + ", "
-        + record[2][1] + "], description: " + record[3]);
+        + record[2][1] + "], description: " + record[3] + ", CRC: " + record[4]);
 }
 
